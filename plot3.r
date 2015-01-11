1192c21939047ae223cdb5fc16d9cabdee1a61a7
ericmerriman <eric_merriman@yahoo.com>
2015-01-11 10:31:45 -0500

    hi

---
+++ plot3.r
@@ -0,0 +1,14 @@
+read.table("household_power_consumption.txt",header=TRUE,sep=";")->abcd
+abcd$DateTime <- as.POSIXct(paste(abcd$Date, abcd$Time), format="%d/%m/%Y %H:%M:%S") 
+##rbind(cbind(as.POSIXct(abcd$DateTime),abcd$Sub_metering_1),cbind(as.POSIXct(abcd$DateTime),abcd$Sub_metering_2),
+##cbind(as.POSIXct(abcd$DateTime),abcd$Sub_metering_3))->bn
+##g<-gl(3,2880,labels=c("Sub_metering_1","Sub_metering_2","Sub_metering_3"))
+plot(abcd$DateTime,abcd$Sub_metering_3,type="n", xlab="",ylab="Energy Sub Metering")
+plot(abcd$DateTime,abcd$Sub_metering_2,type="n", xlab="",ylab="Energy Sub Metering")
+plot(abcd$DateTime,abcd$Sub_metering_1,type="n", xlab="",ylab="Energy Sub Metering")
+lines(abcd$DateTime,abcd$Sub_metering_3,col="blue")
+lines(abcd$DateTime,abcd$Sub_metering_2,col="red")
+lines(abcd$DateTime,abcd$Sub_metering_1,col="black")
+legend("topright",legend=c("Sub_metering_1","Sub_metering_2","Sub_metering_3"),pch=paste("-"),col=c("black","red","blue"))
+dev.copy(png, file = "plot3.png")
+dev.off() 
