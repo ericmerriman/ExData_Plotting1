8a4ebe9bbcc66a4040f3ab89247b4b6f1e46f8cc
ericmerriman <eric_merriman@yahoo.com>
2015-01-18 16:35:00 -0500

    Delete plot2.r

--- plot2.r
+++
@@ -1,6 +0,0 @@
-read.table("household_power_consumption.txt",header=TRUE,sep=";")->abcd
-abcd$DateTime <- as.POSIXct(paste(abcd$Date, abcd$Time), format="%d/%m/%Y %H:%M:%S") 
-plot(abcd$DateTime, abcd$Global_active_power, type="n", xlab="",ylab="Global Active Power (kilowatts)")
-lines(abcd$DateTime, abcd$Global_active_power, type="l")
-dev.copy(png, file = "plot2.png")
-dev.off() 
