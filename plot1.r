7f536f9b3f685ab53afe08bc954c2489c9719521
ericmerriman <eric_merriman@yahoo.com>
2015-01-18 16:34:50 -0500

    Delete plot1.r

--- plot1.r
+++
@@ -1,6 +0,0 @@
-read.table("household_power_consumption.txt",header=TRUE,sep=";")->abcd
-as.Date(abcd$Date, "%d/%m/%Y")=="2007-02-02"|as.Date(abcd$Date, "%d/%m/%Y")=="2007-02-01"
-hist(abcd$Global_active_power,xlab="Global Active Power (kilowatts)",
-     ylab="Frequency", col="red", main = "Global Active Power")->p
-dev.copy(png, file = "plot1.png")
-dev.off() 
